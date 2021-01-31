46bdca16a7f1920631a1476f99cfb3acb8baea4c
Gavin Sharkey <gavinsharkey2016@gmail.com>
2021-01-31 20:04:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function findMinAndRemove(array){
-
+  let minIndex = 0
+  
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < array[minIndex]) {
+      minIndex = i
+    }
+  }
+  
+  return array.splice(minIndex, 1)
 }
 
 function insertionSort(array){
